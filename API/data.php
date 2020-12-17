7c17f2732fd4a4856f1990b0653e1834f135a20e
jikksii <giorgiijikia@gmail.com>
2020-12-17 21:42:54 +0400

    header, navigation impl

---
+++ API/data.php
@@ -0,0 +1,108 @@
+<?php
+$data = [
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+    [
+        "ID" => 1,
+        "Type" => '',
+        "Title" => "",
+        "Artist" =>"",
+        "Price" => "",
+        "Size" => "",
+        "About" => "",
+        "Details" => ""
+    ],
+];
+
+
+header('Content-type: application/json');
+echo json_encode($data);
+?>
---
+++ js/main.js
@@ -0,0 +1,50 @@
+let isLogedIn = function(){
+    return false;
+}
+ 
+let show_Info_bar = function(){
+    if(isLogedIn()){
+        document.getElementById('info-bar-menu-logged').style.visibility = 'visible' 
+    }else{
+        document.getElementById('info-bar-menu-not-logged').style.visibility = 'visible'
+    }
+}
+
+let hide_info_bar = function(){
+    document.getElementById('info-bar-menu-not-logged').style.visibility = 'hidden'
+    document.getElementById('info-bar-menu-logged').style.visibility = 'hidden'
+}
+
+let show_hide_navbar = function(){
+    let navbar = document.getElementById('nav-full')
+    console.log(navbar.style.visibility)
+    // let pos , to , increment
+    // if(navbar.style.visibility === 'hidden'){
+    //     pos = -900;
+    //     to = 0
+    //     increment = 4
+    //     navbar.style.visibility = 'visible'
+    // }else{
+    //     pos = 0;
+    //     to = -900
+    //     increment = -4
+    //     navbar.style.visibility = 'hidden'
+    // }
+    // var id = setInterval(frame, 0);
+    // function frame() {
+    //     if (pos == to) {
+    //         clearInterval(id);
+    //     } else {
+    //         pos+=increment; 
+    //         navbar.style.left = pos + "px"; 
+    //     }
+    // }
+}
+document.getElementById('menu-btn').addEventListener('click',()=>show_hide_navbar())
+
+
+let user_btn = document.getElementById('my-profile');
+user_btn.onmouseover = ()=>show_Info_bar();
+user_btn.onmouseout = ()=>hide_info_bar();
+
+
