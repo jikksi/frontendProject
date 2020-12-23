ab2dd8f0543b8c2d241bf0a27c939497bf837128
jikksi <gijiki16@freeuni.edu.ge>
2020-12-24 02:48:17 +0400

    filters implemented

--- js/main.js
+++ js/main.js
@@ -42,7 +42,59 @@ let show_hide_navbar = function(){
 }
 document.getElementById('menu-btn').addEventListener('click',()=>show_hide_navbar())
 
+document.getElementById('filter-exclusive').addEventListener('click',function(){
+    
+    selectExclusiveItems()
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("filter", "exclusive");
+    window.location.search = searchParams.toString();
 
+})
+document.getElementById('filter-all-item').addEventListener('click',function(){
+   
+    selectAllFilter()
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("filter", "all");
+    window.location.search = searchParams.toString();
+})
+
+function checkQueryParameters(){
+    console.log("check")
+    var urlParams = new URLSearchParams(window.location.search);
+    var keys = urlParams.keys();
+    for(key of keys){
+        console.log(key)
+        if(key == 'filter' && urlParams.get(key) == "exclusive"){
+            console.log('here')
+            selectExclusiveItems()
+        }
+    }
+}
+
+function selectExclusiveItems(){
+    let all_item = document.getElementById('filter-all-item')
+    let exclusive = document.getElementById('filter-exclusive')
+    all_item.classList.remove("filter-by-item-selected");
+    exclusive.classList.add("filter-by-item-selected");
+}
+
+function selectAllFilter(){
+    let all_item = document.getElementById('filter-all-item')
+    let exclusive = document.getElementById('filter-exclusive')
+    exclusive.classList.remove("filter-by-item-selected");
+    all_item.classList.add("filter-by-item-selected");
+}
+
+// document.getElementById('sort-select').addEventListener('change', function() {
+//     var searchParams = new URLSearchParams(window.location.search);
+//     searchParams.set("sort", this.value);
+//     window.location.search = searchParams.toString();
+// });
+
+
+
+
+checkQueryParameters()
 let user_btn = document.getElementById('my-profile');
 user_btn.onmouseover = ()=>show_Info_bar();
 user_btn.onmouseout = ()=>hide_info_bar();
