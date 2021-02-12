255ff4c928dc0614548ecd202381d59baab65dfb
jikksi <gijiki16@freeuni.edu.ge>
2021-02-12 20:13:33 +0400

    login page view implemented

--- API/painting.php
+++ API/painting.php
@@ -452,20 +452,134 @@ $data = [
     ],
     
 ];
+$exclusives = [
+    [
+        "ID" => 9,
+        "Type" => 'The Print',
+        "Title" => "End Of Winter",
+        "Artist" =>"Leonid Afremov",
+        "Price" => 15,
+        "Size" => "16 x 16",
+        "img" => "http://localhost:80/images/9.jpg"
+    ],
+    [
+        "ID" => 3,
+        "Type" => 'The Print',
+        "Title" => "April in Paris",
+        "Artist" =>"Brent Heighton",
+        "Price" => 15,
+        "Size" => "16 x 12",
+        "img" => "http://localhost:80/images/3.jpg"
+    ],
+    [
+        "ID" => 4,
+        "Type" => 'The Print',
+        "Title" => "The Great Wave off Kanagawa",
+        "Artist" =>"Katsushika Hokusai",
+        "Price" => 15,
+        "Size" => "18 x 12",
+        "img" => "http://localhost:80/images/4.jpg"
+    ],
+    [
+        "ID" => 5,
+        "Type" => 'The Print',
+        "Title" => "Untitled, 1981",
+        "Artist" =>"Jean-Michel Basquiat",
+        "Price" => 18,
+        "Size" => "12 x 12",
+        "img" => "http://localhost:80/images/5.jpg"
+    ],
+    [
+        "ID" => 6,
+        "Type" => 'The Print',
+        "Title" => "Chicken for Dinner",
+        "Artist" =>" Lucia Heffernan",
+        "Price" => 30,
+        "Size" => "18 x 24",
+        "img" => "http://localhost:80/images/6.jpg"
+    ],
+    [
+        "ID" => 3,
+        "Type" => 'The Print',
+        "Title" => "April in Paris",
+        "Artist" =>"Brent Heighton",
+        "Price" => 15,
+        "Size" => "16 x 12",
+        "img" => "http://localhost:80/images/3.jpg"
+    ],
+    [
+        "ID" => 4,
+        "Type" => 'The Print',
+        "Title" => "The Great Wave off Kanagawa",
+        "Artist" =>"Katsushika Hokusai",
+        "Price" => 15,
+        "Size" => "18 x 12",
+        "img" => "http://localhost:80/images/4.jpg"
+    ],
+    [
+        "ID" => 5,
+        "Type" => 'The Print',
+        "Title" => "Untitled, 1981",
+        "Artist" =>"Jean-Michel Basquiat",
+        "Price" => 18,
+        "Size" => "12 x 12",
+        "img" => "http://localhost:80/images/5.jpg"
+    ],
+    [
+        "ID" => 6,
+        "Type" => 'The Print',
+        "Title" => "Chicken for Dinner",
+        "Artist" =>" Lucia Heffernan",
+        "Price" => 30,
+        "Size" => "18 x 24",
+        "img" => "http://localhost:80/images/6.jpg"
+    ],
+];
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS");
 header("Access-Control-Allow-Headers:  Content-Type,authorization");
 header('Content-Type: application/json');
 if($_SERVER['REQUEST_METHOD'] === 'GET') {
     global $data;
+    global $exclusives;
     $page = $_GET['page']-1;
-    $data_size = count($data);
+    $filter = null;
+    $sort = null;
+    
+    $array = $data;
+    if(array_key_exists('filter',$_GET)){
+        $filter = $_GET['filter'];
+    }
+    if(array_key_exists('sort',$_GET)){
+        $sort = $_GET['sort'];
+    }
+    if($filter && $filter == 'exclusive'){
+        $array = $exclusives;
+    }
+
+    if($sort){
+        if($sort == 'ltoh'){
+            usort($array,"compareByPriceLTH");
+        }elseif($sort == 'htol'){
+            usort($array,"compareByPriceHTL");
+        }
+    }
+
+    $data_size = count($array);
     $from = $page * 12;
     $to = $from + 12;
-    $max_page = $data_size / 12;
+    $max_page = ceil($data_size / 12);
     echo json_encode([
         "max_page" =>$max_page ,
-        "data"=>array_slice($data,$from ,12)
+        "data"=>array_slice($array,$from ,12)
     ]);
+}
+
+function compareByPriceHTL($item1 , $item2){
+    return $item2['Price'] - $item1['Price'];
+}
+function compareByPriceLTH($item1 , $item2){
+    return $item1['Price'] - $item2['Price'];
 }
 ?>
--- js/main.js
+++ js/main.js
@@ -42,67 +42,6 @@ let show_hide_navbar = function(){
 }
 document.getElementById('menu-btn').addEventListener('click',()=>show_hide_navbar())
 
-document.getElementById('filter-exclusive').addEventListener('click',function(){
-    
-    selectExclusiveItems()
-    var searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("filter", "exclusive");
-    searchParams.set("page",1)
-    window.location.search = searchParams.toString();
-
-})
-
-
-document.getElementById('filter-all-item').addEventListener('click',function(){
-    selectAllFilter()
-    var searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("filter", "all");
-    searchParams.set("page",1)
-    window.location.search = searchParams.toString();
-})
-
-document.getElementById('sort-select').addEventListener('change',function(){
-    var searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("sort", this.value);
-    window.location.search = searchParams.toString();
-})
-
-function checkQueryParameters(){
-    var urlParams = new URLSearchParams(window.location.search);
-    var keys = urlParams.keys();
-    for(key of keys){
-        if(key == 'filter' && urlParams.get(key) == "exclusive"){
-            selectExclusiveItems()
-        }
-        if(key === 'sort'){
-            console.log('here')
-            setSortValue(urlParams.get(key))
-        }
-    }
-}
-
-function setSortValue(value){
-    document.getElementById('sort-select').value = value;  
-}
-
-function selectExclusiveItems(){
-    let all_item = document.getElementById('filter-all-item')
-    let exclusive = document.getElementById('filter-exclusive')
-    all_item.classList.remove("filter-by-item-selected");
-    exclusive.classList.add("filter-by-item-selected");
-}
-
-function selectAllFilter(){
-    let all_item = document.getElementById('filter-all-item')
-    let exclusive = document.getElementById('filter-exclusive')
-    exclusive.classList.remove("filter-by-item-selected");
-    all_item.classList.add("filter-by-item-selected");
-}
-
-
-
-checkQueryParameters()
-
 
 let user_btn = document.getElementById('my-profile');
 user_btn.onmouseover = ()=>show_Info_bar();
